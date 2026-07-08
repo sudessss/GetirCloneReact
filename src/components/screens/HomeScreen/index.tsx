87461178d0dae9bbe02c767e9ad3ec35c3aa60e3
sudesenturk <sudesenturk77@gmail.com>
2026-07-08 14:32:32 +0300

    Banner alanı tamamlandı

--- src/components/screens/HomeScreen/index.tsx
+++ src/components/screens/HomeScreen/index.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import HeaderMain from "../../HeaderMain"
 import { ScrollView } from 'react-native'
 
+import BannerCarousel from '../../BannerCarousel'
 
 
 function Index(){
   return (
-   <ScrollView> <HeaderMain /></ScrollView>
+   <ScrollView stickyHeaderIndices={[0]} style= {{backgroundColor: '#f5f5f5'}}>
+     <HeaderMain />
+   <BannerCarousel/>
+   </ScrollView>
    
    
   )
